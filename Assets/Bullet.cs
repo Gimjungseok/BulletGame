c9f51e6bad426e8fbafdc8a02beea8546d1ceb3d
Gimjungseok <63216469+Gimjungseok@users.noreply.github.com>
2020-06-17 10:04:45 +0900

    2020 06 17
    asdasdas

---
+++ Assets/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    private Rigidbody bulletRigidbody;
+    void Start()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+        bulletRigidbody.velocity = transform.forward * speed;
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayerController PlayerController = other.GetComponent<PlayerController>();
+            if (PlayerController != null)
+            {
+                PlayerController.Die();
+            }
+        }
+    }
+}
